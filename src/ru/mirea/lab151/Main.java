08a518830510078605d8e2ebaf864d2afe8b3b56
Sa0131 <novruz24082015@gmail.com>
2022-12-20 22:11:30 +0300

    Initial commit

---
+++ src/ru/mirea/lab151/Main.java
@@ -0,0 +1,22 @@
+package ru.mirea.lab151;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+        Form1 form = new Form1();
+        form.setVisible(true);
+
+        Form2 form2 = new Form2();
+        form2.setVisible(true);
+
+        Form3 form3 = new Form3();
+        form3.setVisible(true);
+
+        Form4 form4 = new Form4();
+        form4.setVisible(true);
+    }
+}
